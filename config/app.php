b897000a04098b6a180f500ccf9a93286fa2380f
Kened <kened.felix@gmail.com>
2021-08-22 22:43:07 -0300

    Configurando app.php

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://sisagendamentoapi.herokuapp.com/'),
 
     'asset_url' => env('ASSET_URL', null),
 
@@ -119,7 +119,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY','base64:YyS8Ary2K8X8ao1Vc1ZObJl75IpUs6vtoEx1gqYsavA='),
 
     'cipher' => 'AES-256-CBC',
 
